13fd7d6b6931250d0aeee545679a1f9801f9a1c0
Cest <ronaldalvarezjsjs@gmail.com>
2025-09-25 18:54:57 -0500

    añado el main - Ronald Alvare

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Equipo: Ronald Alvarez, Ivan Andres, 
+"""
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    
+    print(f"Suma de {x} + {y} = {sumar(x, y)}")
+    print(f"Resta de {x} - {y} = {restar(x, y)}")
